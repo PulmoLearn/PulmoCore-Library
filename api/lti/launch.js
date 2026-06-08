4ff41a419be7da850193e7f09bcd3e6aaed42486
PulmoLearn <melissa.wells52@gmail.com>
2026-06-08 09:38:00 -0500

    Test jose dynamic import

--- api/lti/launch.js
+++ api/lti/launch.js
@@ -1,9 +1,20 @@
-export default function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).send("Method not allowed");
-  }
+export default async function handler(req, res) {
+  try {
+    if (req.method !== "POST") {
+      return res.status(405).send("Method not allowed");
+    }
 
-  return res.status(200).json({
-    message: "LTI launch endpoint is alive"
-  });
+    const jose = await import("jose");
+
+    return res.status(200).json({
+      message: "LTI launch endpoint is alive",
+      joseLoaded: true,
+      joseKeys: Object.keys(jose).slice(0, 5)
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: "Launch test failed",
+      details: error.message
+    });
+  }
 }
