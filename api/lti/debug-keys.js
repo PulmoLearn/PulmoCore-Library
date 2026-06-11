845c54c1b7998c1e48cac78c1856b995c4044f04
PulmoLearn <melissa.wells52@gmail.com>
2026-06-11 18:23:00 -0500

    Delete debug-keys.js

--- api/lti/debug-keys.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const priv = process.env.LTI_PRIVATE_KEY || ''
-  const pub  = process.env.LTI_PUBLIC_KEY  || ''
-  return res.status(200).json({
-    privateLine1: priv.split('\n')[0],
-    publicLine1:  pub.split('\n')[0],
-  })
-}
